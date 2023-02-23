f0845019bb0e53755709b9f5a8817d28a7173f87
sime3134 <simon_jern@msn.com>
2023-02-23 10:42:54 +0100

    Bugfix keys
    Fixed a bug where an extra row in the api keys file would cause the program to crash.

--- src/main/java/org/example/KeyHandler.java
+++ src/main/java/org/example/KeyHandler.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 /**
  * This class is used to handle the keybindings for the program.
@@ -26,14 +27,15 @@ public class KeyHandler {
                     new BufferedReader(new InputStreamReader(new FileInputStream("api_keys.txt")))) {
             while (true) {
                 String word = r.readLine();
-                if (word == null) {
+                if (word == null || word.isEmpty()) {
                     break;
                 }
-                String[] split = word.split("=");
+                String[] split = word.split(Pattern.quote("="));
                 keys.put(split[0], split[1]);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        System.out.println("Keys read successfully");
     }
 }
